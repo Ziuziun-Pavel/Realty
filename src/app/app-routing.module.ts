61372dcac1128b5304be0f1299be307198311043
Pavel <zuzunpavel@gmail.com>
2022-01-19 22:13:52 +0300

    layout of registration and authorization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FlatPageComponent } from './features/home/components/advertisement/flat-page/flat-page.component';
 import { HomeComponent } from './features/home/home.component';
 import { NewsPageComponent } from './features/home/components/news/news-page/news-page.component';
+// import { AccountModule } from "./account/account.module";
 
 const routes: Routes = [
   {path: 'page/:type/:id', component: FlatPageComponent},
   {path: '', component: HomeComponent},
-  {path: 'news/:id', component: NewsPageComponent}
+  {path: 'news/:id', component: NewsPageComponent},
+  // { path: 'account', component: AccountModule },
+  // { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
