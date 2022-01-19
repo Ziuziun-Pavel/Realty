6129acf08edabc4e89bd45dd94e27dc0766042e9
Pavel <zuzunpavel@gmail.com>
2022-01-19 17:22:13 +0300

    convert json files to .ts

---
+++ src/app/account/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    // this.registerForm = this.formBuilder.group(
+    //   {
+    //
+    //   }
+    // )
+  }
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AccountRoutingModule } from "../account/account-routing.module";
 
 @NgModule({
-    imports:[CommonModule],
+	imports: [CommonModule, AccountRoutingModule],
     declarations: [
         HeaderComponent,
         FooterComponent
