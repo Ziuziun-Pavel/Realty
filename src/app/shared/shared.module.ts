3225e77b49a6727a613bc5930eba280e21f64821
Pavel <zuzunpavel@gmail.com>
2022-02-07 19:14:03 +0300

    search_panel/edit-imports-in-sharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoaderService } from './services/loader.service';
-import { UserFormComponent } from './components/user-form/user-form.component';
 
 @NgModule({
   imports: [CommonModule, MatProgressSpinnerModule],
   declarations: [
     LoaderComponent,
-    UserFormComponent,
   ],
   exports: [LoaderComponent],
   providers: [LoaderService],
