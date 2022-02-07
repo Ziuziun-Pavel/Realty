0141b52db5abbc4cc8328ddf034b058be9723f7d
Pavel <zuzunpavel@gmail.com>
2022-02-07 14:06:48 +0300

    adding_adverts/unsubscribe-from-addAdvert-form

--- src/app/profile/add-adverts/add-adverts.component.ts
+++ src/app/profile/add-adverts/add-adverts.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../core/services/user.service';
 import { ToastrService } from 'ngx-toastr';
+import { LoaderService } from '../../shared/services/loader.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-add-adverts',
   templateUrl: './add-adverts.component.html',
   styleUrls: ['./add-adverts.component.scss']
 })
-export class AddAdvertsComponent implements OnInit {
+export class AddAdvertsComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe: Subject<boolean> = new Subject();
 
-  loading = false;
+  public loading = false;
 
-  submitted = false;
+  public submitted = false;
 
-  addingForm: FormGroup;
+  public addingForm: FormGroup;
 
   constructor(
     private readonly formBuilder: FormBuilder,
     private readonly router:Router,
     private readonly userService: UserService,
+    private readonly loaderService: LoaderService,
     private readonly toastr: ToastrService,
   ) { }
 
@@ -50,21 +54,30 @@ export class AddAdvertsComponent implements OnInit {
   get formControls(): {[key: string]: AbstractControl} { return this.addingForm.controls; }
 
   onSubmit(): void {
-    this.submitted = true;
     if (this.addingForm.invalid) {
       return;
     }
-     this.loading = true;
+    this.submitted = true;
+    this.loading = true;
+    this.loaderService.show();
     this.userService.addNewCard(this.addingForm.value)
+      .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe(
         ()=>{
+          this.loading = false;
+          this.loaderService.hide();
           alert('Объявление добавлено!!');
           this.router.navigate(['/details']);
         },
         (error)=>{
           this.toastr.error(error.error.message, 'Error');
           this.loading = false;
+          this.loaderService.hide();
         },
       );
+  }
+  ngOnDestroy() {
+    this.ngUnsubscribe.next(true);
+    this.ngUnsubscribe.complete();
   }
 }
