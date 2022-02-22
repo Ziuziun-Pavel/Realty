c662ac88ebdeda698b0b31e31a28b46f220b7a64
Pavel <zuzunpavel@gmail.com>
2022-02-22 21:05:26 +0300

    role-based-auth/add-edit-and-delete-methods-to-all-cards

--- src/app/features/home/components/advertisement/rent-cards/rent-cards.component.ts
+++ src/app/features/home/components/advertisement/rent-cards/rent-cards.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CardService } from '../../../services/card.service';
 import { ICard } from '../../../../../core/models/cards';
+import { AuthService } from '../../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-rent-cards',
@@ -12,10 +13,20 @@ import { ICard } from '../../../../../core/models/cards';
 export class RentCardsComponent implements OnInit {
   public cards: Observable<Array<ICard>>;
 
-  constructor(private readonly cardService: CardService) { }
+  constructor(
+    private readonly cardService: CardService,
+    private readonly authService: AuthService,
+    ) { }
 
   ngOnInit(): void {
     this.cards = this.cardService.getRentCards();
   }
 
+  public onDelete(id: string) {
+    this.cardService.deleteCard(id);
+  }
+
+  public isAdmin() {
+    return this.authService.isAdmin();
+  }
 }
--- src/app/features/home/components/advertisement/sell-card/sell-card.component.ts
+++ src/app/features/home/components/advertisement/sell-card/sell-card.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CardService } from '../../../services/card.service';
 import { Observable } from 'rxjs';
 import { ICard } from '../../../../../core/models/cards';
+import { AuthService } from '../../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sell-card',
@@ -12,9 +13,21 @@ import { ICard } from '../../../../../core/models/cards';
 export class SellCardComponent implements OnInit {
   public cards: Observable<Array<ICard>>;
 
-  constructor(private readonly cardService: CardService) {  }
+  constructor(
+    private readonly cardService: CardService,
+    private readonly authService: AuthService,
+  ) {  }
 
   ngOnInit(): void {
     this.cards = this.cardService.getSellCards();
+  }
+
+
+  public onDelete(id: string) {
+    this.cardService.deleteCard(id);
+  }
+
+  public isAdmin() {
+    return this.authService.isAdmin();
   }
 }
