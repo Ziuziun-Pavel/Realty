259b19a0e789378c946fe693202b6a6f36c36376
Pavel <zuzunpavel@gmail.com>
2022-02-22 21:05:01 +0300

    role-based-auth/add-admin-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {
-  FlatPageComponent,
-} from './features/home/components/advertisement/flat-page/flat-page.component';
+import { FlatPageComponent } from './features/home/components/advertisement/flat-page/flat-page.component';
 import { HomeComponent } from './features/home/home.component';
 import { NewsPageComponent } from './features/home/components/news/news-page/news-page.component';
 import { AdminGuard } from './core/guards/admin.guard';
