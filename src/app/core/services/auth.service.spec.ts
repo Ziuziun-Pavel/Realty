0ac3a8382861438717a48d94036b719721892bc3
Pavel <zuzunpavel@gmail.com>
2022-03-07 17:11:34 +0300

    unit-tests/test-of-authService

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,13 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 import { MockAuthService } from './auth.service.mock';
+import { IUser } from '../models/user';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let MockUser: IUser;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes([])],
       providers: [
         {
           provide: AuthService,
@@ -15,10 +22,65 @@ describe('AuthService', () => {
         }
       ]
     });
-    service = TestBed.inject(AuthService);
+    MockUser = {
+      id: '1sd78sdf',
+      userName: 'John',
+      userSurname: 'Smith',
+      userEmail: 'asd@gmail.com',
+      password: '1234567',
+    };
+    router = TestBed.get(Router);
+    authService = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authService).toBeTruthy();
   });
+
+  it('should check if the user is authorized', () => {
+    expect(MockUser).toBeTruthy()
+  });
+
+  it('should check if the user is admin', () => {
+    expect(MockUser.role).toBeUndefined()
+  });
+
+  it('the user should sign in', (done) => {
+    let user;
+    spyOn(authService, 'login').and.returnValue(of(user));
+    authService.login(MockUser).subscribe(() => {
+      user = MockUser;
+      expect(authService.login).toHaveBeenCalledOnceWith(MockUser);
+      expect(user).toEqual(MockUser)
+      done();
+    })
+  });
+
+  it('the user should correctly register', (done) => {
+    let user: IUser[] = [];
+    spyOn(authService, 'register')
+      .and
+      .returnValue(of(user));
+
+    authService.register(MockUser.userName, MockUser.userSurname, MockUser.userEmail, '1234567').subscribe(() => {
+      user.push(MockUser);
+    })
+
+    expect(authService.register).toHaveBeenCalled();
+    expect(user.length).toBe(1)
+
+    done();
+  });
+
+  it('the user should sign out', (done) => {
+    const authServiceSpy = jasmine.createSpyObj('authService', ['logout']);
+    authServiceSpy.logout();
+
+    expect(authServiceSpy.logout).toHaveBeenCalled();
+    expect(localStorage.length).toBe(0);
+    done();
+  });
+
+
+
 });
